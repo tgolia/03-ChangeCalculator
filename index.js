6abcd64555140835c2d6f979c89f0938bca209e4
Travis Golia <travis@gigtown.com>
2016-11-15 16:57:28 -0800

    v4, error messages,code cleanup

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-/*At the last minute, you've been asked to work on team developing a module for an automated check-out system of a nationally based grocery store. The app you're building will accept two inputs:
-
-The total amount of the sale
-The amount tendered by the customer.
-When the user clicks on a Calculate button (you'll need a button too!), your app should calculate the amount of change due in dollars, quarters, dimes, nickels and pennies.
-
-This should give you a visual idea of what is required. Since it is just a prototype mockup, you are free to modify the layout and styles as you see fit.*/
-
 var currencyArray = [
-	{ name : '_50dollars' , amount : 50   , numOf : 0 } ,
+	{ name : '_50dollars' , amount : 50   , numOf : 0 } ,   // array of objects for each currency type
 	{ name : '_20dollars' , amount : 20   , numOf : 0 } ,
 	{ name : '_10dollars' , amount : 10   , numOf : 0 } ,
 	{ name : '_5dollars'  , amount : 5    , numOf : 0 } ,
@@ -19,62 +11,83 @@ var currencyArray = [
 	{ name : '_pennies'   , amount : 0.01 , numOf : 0 }
 ]
 
-function roundToDecimal(num,dec) {        //custom function that rounds number num to decimal dec that you specify
+function roundToDecimal(num,dec) {        // custom function that rounds number num to decimal dec that you specify
 	var rounded = (Math.round(num * Math.pow(10,dec)) / Math.pow(10,dec)).toFixed(dec);
 	return rounded;
 }
 
-function calculateChange() {
+function hideElements() {
+	document.getElementById('dollars').style.visibility='hidden';   // custom function to hide certain elements on the page
+	document.getElementById('coins').style.visibility='hidden';     // much cleaner than using these 3 lines multiple times
+	document.getElementById('captions').style.visibility='hidden';
+}
 
-	var amountDue = document.getElementById('inputAmountDue').value;
+function showElements() {
+	document.getElementById('dollars').style.visibility='visible';   // custom function to show certain elements on the page
+	document.getElementById('coins').style.visibility='visible';     // much cleaner than using these 3 lines multiple times
+	document.getElementById('captions').style.visibility='visible';
+}
 
-	var customerGiven = document.getElementById('inputAmountGiven').value;
+var amountDue = 0;                       // initializing variables that I would like to be universal
+var customerGiven = 0;
+var changeOwedOriginally = 0;
+var changeOwed = 0;
 
-	var changeOwedOriginally = roundToDecimal( customerGiven - amountDue , 2 );
+function calculateChange() {  // organized all of the statements/calculations involved in actually performing the change calculation into this function
 
-	var changeOwed = changeOwedOriginally;
+	document.getElementById('changeOwed').innerHTML = 'We owe $' + changeOwed + ' in change.'; // initial statement printed at top that says how much change is due
 
-	document.getElementById('changeOwed').innerHTML = 'You owe $' + changeOwed + ' in change.';
+	var summary = '';    // initialize a summary string to be added to at the end of the for loop
 
-	var summary = '';
+	for ( var i = 0; i < currencyArray.length ; i++ ) {   // for loop iterates through currency type objects
 
-	for ( var i = 0; i < currencyArray.length ; i++ ) {
-
-		var currentCurrency = currencyArray[i];
+		var currentCurrency = currencyArray[i];          // grabs currency type object
 		var name = currentCurrency.name;
-		var amount = currentCurrency.amount;
+		var amount = currentCurrency.amount;            // these 3 variables make the more complicated calculations easier to read
 		var numOf = currentCurrency.numOf;
 
-		if ( changeOwed >= amount ) {
-			numOf = Math.floor( changeOwed / amount );
-			changeOwed = roundToDecimal( changeOwed - numOf * amount , 2 );
+		if ( changeOwed >= amount ) {                   // if change remaining is greater than a currency's amount, there is at least one of that type of currency needed
+			numOf = Math.floor( changeOwed / amount );  // returns the number of this currency type needed
+			changeOwed = roundToDecimal( changeOwed - numOf * amount , 2 );     // updates the change owed depending on how much change has been given out already
 		}
 
-		if ( amount >= 1 && numOf > 0 ) {
+		if ( amount >= 1 && numOf > 0 ) {        // returns the amount of the currency type needed to the HTML for bills
 			document.getElementById(name).innerHTML = numOf + ' x $' + amount + ' bills = $' + numOf*amount;
-			summary += '$' + numOf*amount + ' + ';
-		} else if ( amount < 1 && numOf > 0 ) {
+			summary += '$' + numOf*amount + ' + ';  // updates summary string
+		} else if ( amount < 1 && numOf > 0 ) {   // returns the amount of the currency type needed to the HTML for coins
 			document.getElementById(name).innerHTML = numOf + ' x ' + amount*100 + '&cent; = ' + numOf*amount*100 + '&cent;';
-			summary += (numOf*amount*100) + '&cent; + ';
-		} else if ( numOf == 0 ) {
-			document.getElementById(name).innerHTML = '<i>None</i>';
+			summary += (numOf*amount*100) + '&cent; + ';   // updates summary string
+		} else if ( numOf == 0 ) {   // if none of that currency type used
+			document.getElementById(name).innerHTML = '<i>None</i>';   // adds 'None' to that currency type's HTML element instead
 		}
 	}
-	summary = summary.slice(0,summary.length-3) + ' = $' + changeOwedOriginally + '!';
-	document.getElementById('dollars').style.visibility='visible';
-	document.getElementById('coins').style.visibility='visible';
-	document.getElementById('summary').innerHTML = summary;
+	summary = summary.slice(0,summary.length-3) + ' = $' + changeOwedOriginally + '!';  // creates final part of summary string (cuts out extra '&cent; + ' and adds '= total change' )
+	document.getElementById('summary').innerHTML = summary; // adds summary string to HTML element
 }
 
-var button = document.getElementById('calculateChange');
+var button = document.getElementById('calculateChange');      // initiates the button variable
 
-button.onclick = function() {
-	calculateChange();
+button.onclick = function() {                   // when button is clicked
+	amountDue = document.getElementById('inputAmountDue').value;     // grab the amountDue
+	customerGiven = document.getElementById('inputAmountGiven').value;  // grab the amount given
+	changeOwedOriginally = roundToDecimal( customerGiven - amountDue , 2 ); // calculate the change owed originally so that I can reference it in the summary statement
+	changeOwed = changeOwedOriginally; // this is the change variable that I will update as I calculate the change
+
+	if ( amountDue == '' || customerGiven == '' ) {  // error message for if either of the fields are blank
+		document.getElementById('changeOwed').innerHTML = 'Please complete both fields above.'; 
+		hideElements(); // custom function to hide certain page elements
+	} else if ( isNaN(amountDue) || isNaN(customerGiven) ) {    // error message for if either of the fields contains a letter
+		document.getElementById('changeOwed').innerHTML = 'No letters or symbols in the text field por favor.';
+		hideElements();  // custom function to hide certain page elements
+	} else {
+		calculateChange();  // this function calculates the amount of each currency type needed for the change and displays it on the page
+		showElements();  // images are hidden initially and will show once the button is clicked
+	}
 }
 
-/*var num50s = 0;
+/*var num50s = 0;         
 var num20s = 0;
-var num10s = 0;
+var num10s = 0;                    Here is my original math before the for loop
 var num5s = 0;
 var num1s = 0;
 var numQuarters = 0;
@@ -82,32 +95,32 @@ var numDimes = 0;
 var numNickels = 0;
 var numPennies = 0;
 
-	num50s = Math.floor( changeOwed / 50 );  
-	changeOwed = changeOwed - 50*num50s;     
- 
-	num20s = Math.floor( changeOwed / 20 );                          
-	changeOwed = roundToDecimal( changeOwed - (20 * num20s) , 2 );     
+num50s = Math.floor( changeOwed / 50 );  
+changeOwed = changeOwed - 50*num50s;     
 
-	num10s = Math.floor( changeOwed / 10 );
-	changeOwed = roundToDecimal( changeOwed - 10 * num10s , 2 );    
+num20s = Math.floor( changeOwed / 20 );                          
+changeOwed = roundToDecimal( changeOwed - (20 * num20s) , 2 );     
 
-	num5s = Math.floor( changeOwed / 5 );
-	changeOwed = roundToDecimal( changeOwed - 5 * num5s , 2 );      
+num10s = Math.floor( changeOwed / 10 );
+changeOwed = roundToDecimal( changeOwed - 10 * num10s , 2 );    
 
-	num1s = Math.floor( changeOwed );                                  
-	changeOwed = roundToDecimal( changeOwed - num1s , 2 );             
+num5s = Math.floor( changeOwed / 5 );
+changeOwed = roundToDecimal( changeOwed - 5 * num5s , 2 );      
 
-	numQuarters = Math.floor( changeOwed / .25 );                        
-	changeOwed = roundToDecimal( changeOwed - .25 * numQuarters , 2 );   
+num1s = Math.floor( changeOwed );                                  
+changeOwed = roundToDecimal( changeOwed - num1s , 2 );             
 
-	numDimes = Math.floor( changeOwed / .1 );                           
-	changeOwed = roundToDecimal( changeOwed - .1 * numDimes , 2 );       
+numQuarters = Math.floor( changeOwed / .25 );                        
+changeOwed = roundToDecimal( changeOwed - .25 * numQuarters , 2 );   
 
-	numNickels = Math.floor( changeOwed / .05 );                         
-	changeOwed = roundToDecimal( changeOwed - .05 * numNickels , 2 );    
+numDimes = Math.floor( changeOwed / .1 );                           
+changeOwed = roundToDecimal( changeOwed - .1 * numDimes , 2 );       
 
-	numPennies = Math.round( changeOwed / .01 );                         
-	changeOwed = roundToDecimal( changeOwed - .01 * numPennies , 2 );    
+numNickels = Math.floor( changeOwed / .05 );                         
+changeOwed = roundToDecimal( changeOwed - .05 * numNickels , 2 );    
+
+numPennies = Math.round( changeOwed / .01 );                         
+changeOwed = roundToDecimal( changeOwed - .01 * numPennies , 2 );    
 
 document.getElementById('changeOwed').innerHTML = 'You owe $' + changeOwed + ' in change.';
 document.getElementById('_50dollars').innerHTML = num50s + ' x $50 bills = $' + num50s*50;
@@ -124,10 +137,4 @@ document.getElementById('coins').style.visibility='visible';
 document.getElementById('summary').innerHTML = '$' + num50s*50 + ' + $' + num20s*20 + ' + $' + num10s*10 + ' + $' + num5s*5 + ' + $'
 											 + num1s + ' + ' + numQuarters*25 +'&cent; + ' + numDimes*10 + '&cent; + ' + numNickels*5
 											 + '&cent; + ' + numPennies*1 + '&cent; = $' + changeOwed + '!';*/
-
-
-
-
-
-
 
